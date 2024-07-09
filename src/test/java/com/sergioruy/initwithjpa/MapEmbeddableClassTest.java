608dac96316e732aa1f9bdfa30e9d354d0f1181a
sergioruydev <sergioruyenator@gmail.com>
2024-07-09 17:03:42 -0300

    add optional

--- src/test/java/com/sergioruy/initwithjpa/MapEmbeddableClassTest.java
+++ src/test/java/com/sergioruy/initwithjpa/MapEmbeddableClassTest.java
@@ -2,6 +2,7 @@ package com.sergioruy.initwithjpa;
 
 import com.sergioruy.EntityManagerTest;
 import com.sergioruy.model.AddressDeliveryOrdered;
+import com.sergioruy.model.Customer;
 import com.sergioruy.model.Ordered;
 import com.sergioruy.model.StatusOrder;
 import org.junit.jupiter.api.Test;
@@ -25,11 +26,14 @@ public class MapEmbeddableClassTest extends EntityManagerTest {
         address.setNumber("123");
         address.setComplement("square 4");
 
+        Customer customer = entityManager.find(Customer.class, 1L);
+
         Ordered order = new Ordered();
         order.setDateOrdered(LocalDateTime.now());
         order.setStatus(StatusOrder.WAITING);
         order.setAmount(new BigDecimal("1000.00"));
         order.setAddressDelivery(address);
+        order.setCustomer(customer);
 
         entityManager.getTransaction().begin();
         entityManager.persist(order);
